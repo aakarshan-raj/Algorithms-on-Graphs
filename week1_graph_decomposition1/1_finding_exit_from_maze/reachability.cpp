4e17279203bfd49b68414e7c0e49b05a66027a79
aakarshan-raj <aakarshan7raj@gmail.com>
2024-02-06 21:40:41 +0530

    two vertex reachability

--- week1_graph_decomposition1/1_finding_exit_from_maze/reachability.cpp
+++ week1_graph_decomposition1/1_finding_exit_from_maze/reachability.cpp
@@ -2,19 +2,33 @@
 #include <vector>
 
 using std::vector;
-using std::pair;
 
-
-int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
-  return 0;
+void explore(vector<vector<int>> adj, int node, vector<bool> &visited)
+{
+  visited[node] = true;
+  for (auto const &x : adj[node])
+  {
+    if (!visited[x])
+    {
+      explore(adj, x, visited);
+    }
+  }
 }
 
-int main() {
+int reach(vector<vector<int>> &adj, int x, int y)
+{
+  vector<bool> visited(adj.size(), false);
+  explore(adj, x, visited);
+  return visited[y] ? 1 : 0;
+}
+
+int main()
+{
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
+  vector<vector<int>> adj(n, vector<int>());
+  for (size_t i = 0; i < m; i++)
+  {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
