bbdb9fcbc3d20300d15c350e67f8999820189d56
aakarshan-raj <aakarshan7raj@gmail.com>
2024-02-10 14:26:56 +0530

    solution 2

--- week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.cpp
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.cpp
@@ -2,19 +2,40 @@
 #include <vector>
 
 using std::vector;
-using std::pair;
 
-int number_of_components(vector<vector<int> > &adj) {
+void explore(vector<vector<int>> adj, vector<int> &visited, int node)
+{
+  visited[node] = true;
+  for (const int &x : adj[node])
+  {
+    if (!visited[x])
+    {
+      explore(adj, visited, x);
+    }
+  }
+}
+
+int number_of_components(vector<vector<int>> &adj)
+{
   int res = 0;
-  //write your code here
+  vector<int> visited(adj.size(),false);
+  for (int i = 0; i < adj.size(); i++)
+  {
+    if(!visited[i]){
+      res++;
+      explore(adj,visited,i);
+    }
+  }
   return res;
 }
 
-int main() {
+int main()
+{
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
+  vector<vector<int>> adj(n, vector<int>());
+  for (size_t i = 0; i < m; i++)
+  {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
